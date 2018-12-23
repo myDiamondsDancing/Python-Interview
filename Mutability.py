5c0f53b58e9eb3449e33adf88977607736b20daf
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-12-23 12:03:47 +0200

    Mutability, id and type

--- Mutability.py
+++ Mutability.py
@@ -28,3 +28,45 @@ print(id(a))   #5540960
 
 
 # All id at your computer will be different from mine
+
+
+# Some differences in functions
+# Mutable objects are called by reference in functions with access to original object
+# Immutable objects are called by value in functions
+
+def updateList(my_list):
+    my_list.append(10)
+    
+n = [1, 2, 3]
+print(id(n))    # 9543512
+
+updateList(n)
+print(n)           # [1, 2, 3, 10]
+print(id(n))    # 9543512
+
+def updateNumber(n):
+    n += 10
+    
+b = 10
+updateNumber(10)
+print(b)   # 10
+
+# Varning!
+# Not all immutable objects actually immutable
+# Some example with tuple
+
+t = (1, 2, 3, [4, 5])
+t[3].append(6)
+print(t)    # (1, 2, 3, [4, 5, 6])
+
+# Something about type
+# type(object) return type of object :)
+
+a = 3
+b = .6
+t = (1, 2)
+
+for i in (a, b, t):
+    print(type(i))  
+
+# If you don't understand smth here, check this : https://medium.com/@meghamohan/mutable-and-immutable-side-of-python-c2145cf72747
